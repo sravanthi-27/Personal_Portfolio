e8ee6b051a4453138a9b39c4a3ffa3913dba6bd4
Bindu <mbsravanthi2006@gmail.com>
2025-08-07 02:38:30 +0530

    Prepare project for deployment

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const { execPath } = require('node:process')
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const path = require('path');
 
+// dotenv configuration
+dotenv.config();
 
-//dotenv configuration
-dotenv.config()
+// rest object
+const app = express();
 
-//rest object
-const app = express()
+// middlewares
+app.use(cors());
+app.use(express.json());
 
-//middlewares
-app.use(cors())
-app.use(express.json())
+// routes
+app.use('/api/v1/portfolio', require('./routes/portfolioRoute'));
 
-//routes
-app.use('/api/v1/portfolio',require('./routes/portfolioRoute'));
+// -------------------- DEPLOYMENT --------------------
+const __dirname1 = path.resolve(); // Needed for correct directory path
 
-//port
-const PORT = process.env.PORT || 8080
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname1, 'client/build')));
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname1, 'client', 'build', 'index.html'));
+  });
+}
+// ----------------------------------------------------
 
-//listen
+// port
+const PORT = process.env.PORT || 8080;
+
+// listen
 app.listen(PORT, () => {
-    console.log(`Server Running On PORT ${PORT}`);    
+  console.log(`✅ Server Running on PORT ${PORT}`);
 });
